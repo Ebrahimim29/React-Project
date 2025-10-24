de822ea95e905b9e066b16780600e370179e8b4b
Ebrahimim29 <Ebrahimim29@gmail.com>
2025-10-24 22:39:30 +0330

    Conditional Rendering & Props

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Fragment } from "react"
 import ClassComponent from "./ClassComponent"
 import FunctionalComponent from "./FunctionalComponent"
+import Car from "./Car";
 
 const App = () =>{
 
@@ -9,6 +10,8 @@ const App = () =>{
       <h4>Components: Class & Function</h4>
       <ClassComponent/>
       <FunctionalComponent/>
+      <Car make="Toyota" model="Camery" year={2022} color="Silver" price="$25,000"/>
+      <Car make="Hyundai" model="Azera" year={2018} color="black" price="$27,000"/>
     </Fragment>
 
   )
--- src/FunctionalComponent.jsx
+++ src/FunctionalComponent.jsx
@@ -1,7 +1,13 @@
 const FunctionalComponent = () => {
+
+    const value = prompt("1 or 2");
+
     return (
         <div>
             FunctionalComponent.....
+            Conditional Rendering:
+            {value === "1" ? <h2>خوش آمدید😎</h2> : <h2>لطفا وارد شوید!😐</h2>}
+            {/* {value === "1" ? <h2>خوش آمدید</h2> : null} */}
         </div>
     );
 };
